95220c9c7188468c8a437c663cdfb3dead9b81de
Alex Tiernan-Berry <a.tiernan-berry@leads.io>
2024-06-21 02:26:25 +0100

    Add the index and /room pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Index</h1>
+    <div class="flex justify-center mt-10">
+        <Card class="w-1/2">
+            <CardHeader>
+                <CardTitle>Planning Poker</CardTitle>
+            </CardHeader>
+            <CardContent>
+                What are you doing here? You probably want to <NuxtLink to="/room" class="text-primary underline">create a room</NuxtLink>
+            </CardContent>
+        </Card>
+    </div>
 </template>
---
+++ pages/room/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { v4 as uuidv4 } from 'uuid';
+
+async function createRoom() {
+    const roomId = uuidv4();
+    await navigateTo(`/room/${roomId}`);
+}
+</script>
+
+<template>
+    <div class="flex justify-center mt-10">
+        <Card class="m-10 w-1/2">
+            <CardHeader>
+                <CardTitle>Create a Room</CardTitle>
+            </CardHeader>
+            <CardContent>
+                Start a new session for your team effortlessly. Invite your members and begin estimating in seconds! Simplify your planning process with a dedicated room for each project.
+            </CardContent>
+            <CardFooter class="flex justify-between">
+                <Button @click="createRoom">Create</Button>
+            </CardFooter>
+        </Card>
+    </div>
+</template>
